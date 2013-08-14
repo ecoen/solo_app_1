456c239d49715b0d6a98aede58686f97c9f858e4
ecoen <coenelizabeth@gmail.com>
2013-08-13 21:46:52 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoloApp1::Application.config.secret_key_base = '0c49030df20b6a66e6e54336ba2a66c6f4d3691be70eafe6027a5361dda6c7e72dcf94f6bdca69be2155da704ccda22498b40f6830726a810030dada74d007a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SoloApp1::Application.config.session_store :cookie_store, key: '_solo_app_1_session'
